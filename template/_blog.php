c7da6b48b6b4e24d767347a0216912658cb703cd
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-11-15 23:49:22 +0700

    fix blog

--- template/_blog.php
+++ template/_blog.php
@@ -6,25 +6,25 @@
             <div class="item">
                 <div class="card border-0 font-rubik mr-5" style="width: 18rem;">
                     <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
-                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
-                    <a href="https://www.w3schools.com/" class="color-second text-left">Go somewhere</a>
+                    <img src="./assets/blog/find_x8.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Ngày ra mắt toàn cầu của OPPO Find X8 series đã được xác nhận</p>
+                    <a href="https://fptshop.com.vn/tin-tuc/tin-moi/ngay-ra-mat-toan-cau-cua-oppo-find-x8-series-161428" class="color-second text-left">Go to page</a>
                 </div>
             </div>
             <div class="item">
                 <div class="card border-0 font-rubik mr-5" style="width: 18rem;">
                     <h5 class="card-title font-size-16">Blog Profile</h5>
                     <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
-                    <a href="https://phamhuuloc219.github.io" class="color-second text-left">Go somewhere</a>
+                    <p class="card-text font-size-14 text-black-50 py-1">Profile PHL</p>
+                    <a href="https://phamhuuloc219.github.io" class="color-second text-left">Go to page</a>
                 </div>
             </div>
             <div class="item">
                 <div class="card border-0 font-rubik mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16"> Dev upload Web</h5>
-                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
-                    <a href="https://www.w3schools.com/" class="color-second text-left">Go somewhere</a>
+                    <h5 class="card-title font-size-16"> Become a good developer</h5>
+                    <img src="./assets/blog/220px-W3Schools_logo.svg.webp" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Learn everything about information technology knowledge</p>
+                    <a href="https://www.w3schools.com/" class="color-second text-left">Go to page</a>
                 </div>
             </div>
         </div>
