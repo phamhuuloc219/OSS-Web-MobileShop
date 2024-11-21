791b4be3def7f389a85aef2e55e7d60bdc8a3245
thanhthao29032003 <you@example.com>
2024-11-21 22:18:58 +0700

    fix product

--- template/_blogs.php
+++ template/_blogs.php
@@ -8,7 +8,7 @@
         <div class="owl-carousel owl-theme">
             <div class="item">
                 <div class="card border-0 font-roboto mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <h5 class="card-title font-size-16">Sắp ra mắt</h5>
                     <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
                     <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
                     <a href="#" class="color-second text-left">Đi đến trang</a>
@@ -16,7 +16,7 @@
             </div>
             <div class="item">
                 <div class="card border-0 font-roboto mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16">Blog Profile</h5>
+                    <h5 class="card-title font-size-16">Hồ sơ</h5>
                     <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
                     <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
                     <a href="#" class="color-second text-left">Đi đến trang</a>
@@ -24,9 +24,9 @@
             </div>
             <div class="item">
                 <div class="card border-0 font-roboto mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16"> Dev upload Web</h5>
+                    <h5 class="card-title font-size-16"> Deploy Web</h5>
                     <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
+                    <p class="card-text font-size-14 text-black-50 py-1">Để triển khai một website, đầu tiên bạn cần chọn nền tảng hosting phù hợp như Netlify, Vercel cho frontend, hoặc Heroku, DigitalOcean cho backend. Sau đó, đảm bảo mã nguồn của bạn đã ổn định, các lỗi đã được khắc phục và cấu hình môi trường (như .env) đã chính xác. Cuối cùng, bạn chỉ cần đẩy mã lên GitHub, kết nối với nền tảng hosting đã chọn và cấu hình môi trường live như domain, SSL để website có thể hoạt động trực tuyến.</p>
                     <a href="#" class="color-second text-left">Đi đến trang</a>
                 </div>
             </div>
