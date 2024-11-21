12079135fcc8b93b2e4c6909545b09f59d8b7a89
thanhthao29032003 <you@example.com>
2024-11-21 21:54:15 +0700

    language switch

--- template/_blogs.php
+++ template/_blogs.php
@@ -2,7 +2,7 @@
 <!-- Blogs -->
 <section id="blogs">
     <div class="container py-4">
-        <h4 class="font-roboto font-size-20">Latest Blogs</h4>
+        <h4 class="font-roboto font-size-20"><b>Blog mới nhất</b></h4>
         <hr>
 
         <div class="owl-carousel owl-theme">
@@ -11,7 +11,7 @@
                     <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
                     <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
                     <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
-                    <a href="#" class="color-second text-left">Go somewhere</a>
+                    <a href="#" class="color-second text-left">Đi đến trang</a>
                 </div>
             </div>
             <div class="item">
@@ -19,7 +19,7 @@
                     <h5 class="card-title font-size-16">Blog Profile</h5>
                     <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
                     <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
-                    <a href="#" class="color-second text-left">Go somewhere</a>
+                    <a href="#" class="color-second text-left">Đi đến trang</a>
                 </div>
             </div>
             <div class="item">
@@ -27,7 +27,7 @@
                     <h5 class="card-title font-size-16"> Dev upload Web</h5>
                     <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
                     <p class="card-text font-size-14 text-black-50 py-1">Pain itself is a lot more interesting. This is not the time to pursue the truth and desire for what is right. We often want to incriminate these women.</p>
-                    <a href="#" class="color-second text-left">Go somewhere</a>
+                    <a href="#" class="color-second text-left">Đi đến trang</a>
                 </div>
             </div>
         </div>
