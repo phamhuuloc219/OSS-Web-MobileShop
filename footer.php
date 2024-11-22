f9f11a842e93fb85b87402ab1b23e709321b3f3f
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-11-22 10:59:10 +0700

    fixed footer

--- footer.php
+++ footer.php
@@ -3,111 +3,159 @@
 
 <!-- start #footer -->
 <footer id="footer" class="bg-dark text-white py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-15">
-            <h4 class="font-roboto font-size-20">Mobile Shop</h4>
-            <p class="font-size-14 font-roboto text-white-50">
-              Please give me a call when you feel difficult or have questions
-            </p>
-          </div>
-          <div class="col-lg-4 col-15">
-            <h4 class="font-roboto font-size-20">Newsletter</h4>
-            <form class="form-row">
-              <div class="col">
-                <input type="text" class="form-control" placeholder="Email *" />
-              </div>
-              <div class="col">
-                <button type="submit" class="btn btn-primary mb-2">
-                  Subscribe
-                </button>
-              </div>
-            </form>
-          </div>
-          <div class="col-lg-2 col-15">
-            <h4 class="font-roboto font-size-20">Information</h4>
-            <div class="d-flex flex-column flex-wrap">
-              <a
-                href="https://phamhuuloc219.github.io"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >About Us</a
-              >
-              <a
-                href="https://phamhuuloc219.github.io"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Delivery Information</a
-              >
-              <a
-                href="https://phamhuuloc219.github.io"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Privacy Policy</a
-              >
-              <a
-                href="https://phamhuuloc219.github.io"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Terms & Conditions</a
-              >
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-15">
+        <h4 class="font-roboto font-size-20">Mobile Shop</h4>
+        <p class="font-size-14 font-roboto text-white-50">
+          Hãy gọi cho tôi khi bạn cảm thấy khó khăn hoặc có thắc mắc
+        </p>
+      </div>
+
+      <div class="col-lg-2 col-15">
+        <h4 class="font-roboto font-size-20">Thông tin</h4>
+        <div class="d-flex flex-column flex-wrap">
+          <a
+            href="https://phamhuuloc219.github.io"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Về chúng tôi</a>
+          <a
+            href="https://phamhuuloc219.github.io"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Thông tin giao hàng</a>
+          <a
+            href="https://phamhuuloc219.github.io"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Chính sách bảo mật</a>
+          <a
+            href="https://phamhuuloc219.github.io"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Điều khoản & Điều kiện</a>
+        </div>
+      </div>
+
+
+      <div class="col-lg-2 col-15">
+        <h4 class="font-roboto font-size-20">Tài khoản</h4>
+        <div class="d-flex flex-column flex-wrap">
+          <a
+            href="https://phamhuuloc219.github.io"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Hồ sơ cá nhân</a>
+          <a
+            href="mailto:phamhuuloc2192003@gmail.com"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Email</a>
+          <a
+            href="tel:0376282119"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Hót lai</a>
+          <a
+            href="https://www.facebook.com/phloc2193/"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Facebook</a>
+          <a
+            href="https://www.instagram.com/21st_coluuhmahp_sep/"
+            class="font-roboto font-size-14 text-white-50 pb-1"
+            target="_blank">Instagram</a>
+        </div>
+      </div>
+      <div class="col-lg-4 col-15">
+
+        <div class="px-4 pb-4">
+          <h4 class="font-roboto font-size-20">Hỗ trợ thanh toán</h4>
+          <div class="row no-gutters">
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="VISA" src="https://cdn2.fptshop.com.vn/svg/visa_icon_44fe6e15ed.svg?w=96&amp;q=100">
             </div>
-          </div>
-          <div class="col-lg-2 col-15">
-            <h4 class="font-roboto font-size-20">Account</h4>
-            <div class="d-flex flex-column flex-wrap">
-              <a
-                href="https://phamhuuloc219.github.io"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Profile Forme</a
-              >
-              <a
-                href="mailto:phamhuuloc2192003@gmail.com"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Email</a
-              >
-              <a
-                href="tel:0376282119"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Phone</a
-              >
-              <a
-                href="https://www.instagram.com/21st_coluuhmahp_sep/"
-                class="font-roboto font-size-14 text-white-50 pb-1"
-                target="_blank"
-                >Instagram</a
-              >
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="MasterCard" src="https://cdn2.fptshop.com.vn/svg/mastercard_icon_c75f94f6a5.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="JCB" src="https://cdn2.fptshop.com.vn/svg/jcb_icon_214783937c.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="American Express" src="https://cdn2.fptshop.com.vn/svg/amex_icon_d6fb68108d.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="VNPAY" src="https://cdn2.fptshop.com.vn/svg/vnpay_icon_f42045057d.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="ZaloPay" src="https://cdn2.fptshop.com.vn/svg/zalopay_icon_26d64ea93f.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="ATM" src="https://cdn2.fptshop.com.vn/svg/napas_icon_94d5330e3c.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Kredivo" src="https://cdn2.fptshop.com.vn/svg/kredivo_icon_04f72baf36.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="MoMo" src="https://cdn2.fptshop.com.vn/svg/momo_icon_baef21b5f7.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Foxpay" src="https://cdn2.fptshop.com.vn/svg/foxpay_icon_063b36c1f8.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="AlePay" src="https://cdn2.fptshop.com.vn/svg/alepay_icon_20d5310617.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Muadee" src="https://cdn2.fptshop.com.vn/svg/muadee_icon_5e297d9e61.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Home PayLater" src="https://cdn2.fptshop.com.vn/svg/homepaylater_icon_adef600842.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Apple Pay" src="https://cdn2.fptshop.com.vn/svg/applepay_icon_cb6806a0d0.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Samsung Pay" src="https://cdn2.fptshop.com.vn/svg/samsungpay_icon_0292aa9876.svg?w=96&amp;q=100">
+            </div>
+            <div class="col-3 mb-2">
+              <img class="img-fluid" alt="Google Pay" src="https://cdn2.fptshop.com.vn/svg/googlepay_icon_afa293cc14.svg?w=96&amp;q=100">
             </div>
           </div>
         </div>
+        <div class="border-top pt-4 px-4">
+          <p class="mb-3 font-weight-bold">Chứng nhận</p>
+          <div class="d-flex flex-wrap">
+            <img class="img-fluid mr-2 mb-2" alt="DMCA" src="https://cdn2.fptshop.com.vn/svg/dmca_icon_8fc6622bd5.svg?w=128&amp;q=100">
+            <img class="img-fluid mr-2 mb-2" alt="Thương hiệu mạnh Việt Nam 2013" src="https://cdn2.fptshop.com.vn/svg/thuong_hieu_manh_2013_icon_b56f772475.svg?w=128&amp;q=100">
+            <img class="img-fluid mr-2 mb-2" alt="Sản phẩm - Dịch vụ hàng đầu Việt Nam 2014" src="https://cdn2.fptshop.com.vn/svg/san_pham_dich_vu_hang_dau_viet_nam_icon_282a9ba4f7.svg?w=128&amp;q=100">
+            <img class="img-fluid mr-2 mb-2" alt="Nói không với hàng giả" src="https://cdn2.fptshop.com.vn/svg/noi_khong_voi_hang_gia_icon_e16037d9cb.svg?w=64&amp;q=100">
+            <a rel="nofollow" target="_blank" href="http://online.gov.vn/Home/WebDetails/21883">
+              <img class="img-fluid mr-2 mb-2" alt="Đã thông báo Bộ Công Thương" src="https://cdn2.fptshop.com.vn/svg/da_thong_bao_bo_cong_thuong_icon_64785fb3f7.svg?w=180&amp;q=100">
+            </a>
+          </div>
+        </div>
       </div>
-    </footer>
-    <div class="copyright text-center bg-dark text-white py-2">
-      <p class="font-roboto font-size-14">
-        &copy; Copyrights 2024. Designer PHL
-        <a
-          href="https://phamhuuloc219.github.io"
-          class="color-second"
-          target="_blank"
-          >PHL</a
-        >
-      </p>
+      <div class="w-100 col-md-3">
+
+      </div>
+
     </div>
-    <!-- !start #footer -->
+  </div>
+</footer>
+<div class="copyright text-center bg-dark text-white py-2">
+  <p class="font-roboto font-size-14">
+    &copy; Copyrights 2024. Designer <a
+      href="https://phamhuuloc219.github.io"
+      class="color-second"
+      target="_blank">PHL</a>
+  </p>
+</div>
+<!-- !start #footer -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <!-- Owl Carousel Js file -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha256-pTxD+DSzIwmwhOqTFN+DB+nHjO4iAsbgfyFq5K5bcE0=" crossorigin="anonymous"></script>
+<!-- Owl Carousel Js file -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha256-pTxD+DSzIwmwhOqTFN+DB+nHjO4iAsbgfyFq5K5bcE0=" crossorigin="anonymous"></script>
 
-    <!--  isotope plugin cdn  -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha256-CBrpuqrMhXwcLLUd5tvQ4euBHCdh7wGlDfNz8vbu/iI=" crossorigin="anonymous"></script>
-    <!-- Custom Javascript -->
-    <script src="index.js"></script>
-  </body>
+<!--  isotope plugin cdn  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha256-CBrpuqrMhXwcLLUd5tvQ4euBHCdh7wGlDfNz8vbu/iI=" crossorigin="anonymous"></script>
+<!-- Custom Javascript -->
+<script src="index.js"></script>
+</body>
+
 </html>
