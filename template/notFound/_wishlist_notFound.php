3df0add0c5f04524424c2c76317637269cab8815
thanhthao29032003 <you@example.com>
2024-11-22 11:47:26 +0700

    fix card not found

--- template/notFound/_wishlist_notFound.php
+++ template/notFound/_wishlist_notFound.php
@@ -2,16 +2,16 @@
 
 <section id="cart" class="py-3 mb-5">
     <div class="container-fluid w-75">
-        <h5 class="font-baloo font-size-20">Wish list</h5>
+        <h5 class="font-baloo font-size-20">Sản phẩm yêu thích</h5>
 
         <!--  shopping cart items   -->
         <div class="row">
             <div class="col-sm-9">
                 <!-- Empty Cart -->
-                <div class="row border-top py-3 mt-3">
+                <div class="row border-top py-3 mt-3 d-flex justify-content-center align-items-center">
                     <div class="col-sm-15 text-center py-2">
                         <img src="template/notFound/empty_cart.png" alt="Empty Cart" class="img-fluid" style="height: 200px;">
-                        <p class="font-baloo font-size-16 text-black-50">Empty Wish list</p>
+                        <p class="font-baloo font-size-16 text-black-50">Danh sách yêu thích trống</p>
                     </div>
                 </div>
                 <!-- .Empty Cart -->
